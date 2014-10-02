84e4d6ff23b450d9041d076c0bd1e842443ca564
chrisorwa <chrisorwa@gmail.com>
2014-10-02 14:05:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,34 +3,37 @@ Created 13 August, 2014
 @author : iHub Research
 
 '''
+
 # load required libraries
+from __future__ import division
 import image_processing
 import numpy as np
-from flask import Flask
 
-# create flask web server
-app = Flask(__name__)
+
+# create flask web server 
+#app = Flask(__name__)
 
 # capture HTTP endpoint
-@app.route('/ImPro/<road>')
+#@app.route('/ImPro/<road>')
 
-def get_route(road):
-	# create Directory
-	route = image_processing.Route(road)
-	route.set_dir()
+#def get_route(road):
+# create Directory
+road = 'ojijo'
+route = image_processing.Route(road)
+route.set_dir()
 
-	# grab Image  
-	route.images()
+# grab Image  
+route.images()
 
-	# load images
-	x =route.load()
+# load images
+x =route.load()
 
-	# motion detection
-	y = route.motion(x['img_a.jpg'],x['img_b.jpg'],x['img_c.jpg'])
+# detect motion 
+y = route.motion(x['img_a.jpg'],x['img_b.jpg'],x['img_c.jpg'])
+print y 
 
-	return y
+#if __name__ == "__main__":
+    #app.run()
 
-if __name__ == "__main__":
-    app.run(host='192.168.33.71')
 
 
